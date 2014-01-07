139e9d5b849039196e0d468627f8dfcd3b394984
CarlosVEP <carlos.vep@gmail.com>
2014-01-06 18:17:00 -0800

    index logi
    es el index del logi

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Logi</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+</head>
+<body>
+	<div class="contentLeft">
+		<img src="images/menu.png" alt="icono_menu" />
+		<span class="title">Recent Applications</span>
+		<div class="listaLeft">
+			<li>Dashboard300</li>
+			<li>Dashboard300</li>
+			<li>Dashboard300</li>
+			<li>Dashboard300</li>
+			<li>Dashboard300</li>
+			<li>Dashboard300</li>
+			<li>Dashboard300</li>
+		</div>
+		<div class="listaLeft fondo">
+			<img src="images/resource.png" alt="resource" />
+			<span class="title">Resources</span>
+			<div class="sep"></div>
+			<img src="images/info.png" alt="info" />
+			<a href="#"><span class="subTitle">Current Realease Info</span><br /><span class="descrip">Do you have the current release of your Logi product? Check here for the latest release numbers.</span></a>
+			<div class="sep"></div>
+			<img src="images/logi.png" alt="logi" />
+			<a href="#"><span class="subTitle">Using Logi Studio</span><br /><span class="descrip">Learn how to use Logi Studio and take advantage of its wizards and other advanced features.</span></a>
+			<div class="sep"></div>
+			<img src="images/report.png" alt="report" />
+			<a href="#"><span class="subTitle">Basic Report Tutorial</span><br /><span class="descrip">Get started developing reports with Logi Studio by building a basic report that presents data in a table, using Studio's powerful wizards.</span></a>
+			<div class="sep"></div>
+			<img src="images/debug.png" alt="debug" />
+			<a href="#"><span class="subTitle">Debugging Reports</span><br /><span class="descrip">In case your report doesn't work as you expect it to, these debugging techniques will allow you to inspect the data stream and determine what's going on inside your application.</span></a>
+			<div class="sep"></div>
+			<img src="images/aplication.png" alt="aplication" />
+			<a href="#"><span class="subTitle">Sample Applications</span><br /><span class="descrip">Follow this link to a gallery of sample applications that offer examples you can emulate. It's easy: download, open, and run.</span></a>
+		</div>
+	</div>
+	<div class="contentRight">
+		<img src="images/cloud.png" alt="cloud" />
+		<span class="title">Developer Network</span>
+		<div class="sep"></div>
+		<span class="subTitle">Put "Trendy" Back in Your Reports with Sparklines</span><br />
+		<span class="descrip">Sparklines are tiny charts, useful for showing many trends at once, as a set of timelines, and they're in our latest release, v10.2.314:</span>
+		<img src="images/grafica.png" alt="grafica" />
+		<span class="descrip">Three of our Sparkline varieties are shown above. They're typically drawn without axes or gridlines, often inside data tables, alongside labels.</span>
+		<a href="#">Learn more >></a>
+		<div class="sep"></div>
+		<span class="subTitle">Where's the Documentation!?</span><br />
+		<span class="descrip">The LogiXML Developer Network is an active community of developers and your source for the documentation and other resources that help developers succeed.</span>
+		<a href="#">Visit DevNet now >></a>
+		<div class="sep"></div>
+		<span class="subTitle">Featured Element</span><br />
+		<span class="descrip"><p>Chart "Wait Icon" Now Configurable</p> 
+		Charts have an automatic loading image, a "wait icon", associated with them which appears by default:</span>
+	</div>
+</body>
+</html>
